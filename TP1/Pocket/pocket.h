8354ba91eb6d6c6100cd27ab9a3f1e60f7ebbaa9
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-11-04 16:22:15 +0100

    ignore

---
+++ TP1/Pocket/pocket.h
@@ -0,0 +1,55 @@
+#define loop(i, a, b) for(int i=a;i<b;i++)
+#include <bits/stdc++.h>
+#include "../DataStructure/Matrix.h"
+using namespace std;
+
+class Pocket
+{
+public:
+    Matrix<double> w;
+    Matrix<double> X;
+    Matrix<int> y;
+    int n;
+
+    explicit Pocket(int n)
+    {
+        w = Matrix<double>(n + 1, 1);
+        w.randomfill(&randomGen);
+    }
+
+    double predict(Matrix<double> x)
+    {
+        return double(x * w[ {1, -1}] + w(0));
+    }
+
+    double loss(Matrix<double> w,Matrix<double> &X, Matrix<int> &y)
+    {
+        double loss = 0;
+        auto t = X * w[ {1, -1}] + w(0);
+        loop(i, 0, t.getRows())
+        {
+            if (y(i) * t(i) < 0)
+                loss++;
+        }
+        return loss / X.getRows();
+    }
+
+    vector<double> fit(Matrix<double> &X, Matrix<int> &y)
+    {
+        vector<double> losses;
+        double los = 1;
+        int Tmax = 10000;
+        Matrix<double> ws = w;
+
+        loop(i, 0, Tmax)
+        {
+            // Calculate w(t) TODO
+            if(loss(w,X,y) < loss(ws,X,y))
+                ws = w;
+
+            losses.push_back(loss(ws,X,y));
+        }
+        return losses;
+    }
+
+};
