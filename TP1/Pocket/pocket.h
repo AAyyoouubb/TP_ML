7523b31e5c6c3403df82f982a091e0200ba7e73e
AAyyoouubb <th3assis@gmail.com>
2020-11-04 15:41:30 +0000

    this

--- TP1/Pocket/pocket.h
+++ TP1/Pocket/pocket.h
@@ -12,11 +12,10 @@ double randomGen(int i, int j) {
 class Pocket {
 public:
     Matrix<double> w;
-    Matrix<double> X;
-    Matrix<int> y;
     int n;
 
-    Pocket(int n) {
+    explicit Pocket(int n) {
+        this->n = n;
         w = Matrix<double>(n + 1, 1);
         w.randomfill(&randomGen);
     }
@@ -25,7 +24,7 @@ public:
         return double(x * w[{1, -1}] + w(0));
     }
 
-    double loss(Matrix<double> w, Matrix<double> &X, Matrix<int> &y) {
+    static double loss(Matrix<double> w, Matrix<double> &X, Matrix<int> &y) {
         double loss = 0;
         auto t = X * w[{1, -1}] + w(0);
         loop(i, 0, t.getRows()) {
@@ -41,7 +40,7 @@ public:
         int Tmax = 10000;
         Matrix<double> ws = w;
 
-        loop(i, 0, Tmax) {
+        loop(tt, 0, Tmax) {
             auto t = X * w[{1, -1}] + w(0);
             loop(i, 0, n) {
                 if (y(i) * t(i) < 0) {
